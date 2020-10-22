63ebfc239f805655736738813ea4697840451679
ghemsley <ghemsley@protonmail.ch>
2020-10-22 10:43:53 -0500

    commit tree f790e8f47062bef861e0d4f38a52940fd37cb1db

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(line_array)
+  if line_array.length.zero?
+    puts 'The line is currently empty.'
+  else
+    line_array_copy = line_array
+    line_array_copy = line_array_copy.collect do |name|
+      line_array_copy[line_array_copy.index(name)] = (line_array_copy.index(name) + 1).to_s + ". #{name}"
+    end
+    puts 'The line is currently: ' + line_array_copy.join(' ')
+  end
+end
+
+def take_a_number(line_array, name)
+  line_array.push(name)
+  puts "Welcome, #{name}. You are number #{line_array.index(name) + 1} in line."
+end
+
+def now_serving(line_array)
+  if line_array.length.zero?
+    puts 'There is nobody waiting to be served!'
+  else
+    puts "Currently serving #{line_array.shift}."
+  end
+end
